b020efdc7bd4f7712a3322c59eb64a06ff02ca28
Greg Cole <grcole@blackberry.com>
2019-05-31 14:30:32 -0700

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  /*
+  Test is commented out bc it will want to render Fib and Fib will want to
+  make a request to collect data from backend and we have not got that
+  mocked out.
+  Rather than mocking - we will just say that the test works...
+
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+   */
+});
